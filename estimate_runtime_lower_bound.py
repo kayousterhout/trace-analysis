ebbc05fd5ece61231755d3d06553cf3dd9390a5f
Kay Ousterhout <kayousterhout@gmail.com>
2014-09-05 11:30:00 -0700

    Fixes to code to estimate runtime lower bound.
    This commit refactors the code so it can more easily be
    called across all of the files in a directory, and correctly
    handles in-memory input data when computing the bound.

--- estimate_runtime_lower_bound.py
+++ estimate_runtime_lower_bound.py
@@ -5,23 +5,25 @@ import parse_logs
 NUM_MACHINES = 5
 DISKS_PER_MACHINE = 2
 CPUS_PER_MACHINE = 8
-# Estimate of upper bound on disk throughput
-DISK_MB_PER_SECOND = 100
+# Estimate of reasonable disk throughput
+DISK_MB_PER_SECOND = 50
 
 # Based on EC2's advertised 1000Mbps. These seem to be full duplex links.
 # I've observed MB/s as high as 140.
 NETWORK_MB_PER_SECOND = 125
 
-def main(argv):
-  filename = argv[0]
+def estimate(filename):
   analyzer = parse_logs.Analyzer(filename)
 
   total_job_runtime = 0
   actual_job_runtime = 0
+  min_job_cpu_millis = 0
+  min_job_network_millis = 0
+  min_job_disk_millis = 0
   for id in sorted(analyzer.stages.keys(), reverse=True):
     stage = analyzer.stages[id]
     total_cpu_milliseconds = 0
-    total_input_data = 0
+    total_disk_input_data = 0
     total_hdfs_output_data = 0
     total_remote_mb_read = 0
     total_shuffle_write_mb = 0
@@ -30,28 +32,29 @@ def main(argv):
 
     for task in stage.tasks:
       total_cpu_milliseconds += task.process_cpu_utilization * task.executor_run_time
-      total_input_data += task.input_mb
+      if task.input_read_method != "Memory":
+        total_disk_input_data += task.input_mb
       total_shuffle_write_mb += task.shuffle_mb_written
       total_machine_time_spent += task.executor_run_time
       total_runtime_incl_delay += task.runtime()
       total_hdfs_output_data += task.output_mb
       if task.has_fetch:
         total_remote_mb_read += task.remote_mb_read
-        total_input_data += task.local_mb_read
+        total_disk_input_data += task.local_mb_read
     print "*******************Stage runtime: ", stage.finish_time() - stage.start_time
 
     print "Total millis across all tasks: ", total_machine_time_spent
     print "Total millis including scheduler delay: ", total_runtime_incl_delay
     print "Total CPU millis: ", total_cpu_milliseconds
     min_cpu_milliseconds = total_cpu_milliseconds / (NUM_MACHINES * CPUS_PER_MACHINE)
-    print "Total input MB: ", total_input_data
+    print "Total input MB: ", total_disk_input_data
     print "Total remote MB: ", total_remote_mb_read
     print "Total output MB: ", total_hdfs_output_data
-    total_input_disk_milliseconds = 1000 * total_input_data / DISK_MB_PER_SECOND
+    total_input_disk_milliseconds = 1000 * total_disk_input_data / DISK_MB_PER_SECOND
     # TODO: This doesn't include logging for the disk output size, which currently needs to be done
     # manually.
     print "Total shuffle write MB: ", total_shuffle_write_mb
-    total_output_disk_milliseconds = ((total_shuffle_write_mb + total_hdfs_output_data) /
+    total_output_disk_milliseconds = 1000* ((total_shuffle_write_mb + total_hdfs_output_data) /
       DISK_MB_PER_SECOND)
 
     min_disk_milliseconds = ((total_input_disk_milliseconds + total_output_disk_milliseconds) /
@@ -69,9 +72,24 @@ def main(argv):
     print "Min stage runtime: ", min_stage_runtime
     total_job_runtime += min_stage_runtime
     actual_job_runtime += stage.finish_time() - stage.start_time
-  print "Total job runtime:", total_job_runtime, "milliseconds" 
+
+    min_job_cpu_millis += min_cpu_milliseconds
+    min_job_network_millis += min_network_milliseconds
+    min_job_disk_millis += min_disk_milliseconds
+
+  print "--------------------------------------------------------------"
+  print "Total pipelined job runtime:", total_job_runtime, "milliseconds"
+  total_not_pipelined_runtime = min_job_cpu_millis + min_job_network_millis + min_job_disk_millis
+  print "Total not pipelined job runtime:", total_not_pipelined_runtime, "milliseconds"
+  print "Min CPU milliseconds for job", min_job_cpu_millis, "milliseconds"
+  print "Min network milliseconds for job", min_job_network_millis, "milliseconds"
+  print "Min disk milliseconds for job", min_job_disk_millis, "milliseconds"
   print "Actual job runtime:", actual_job_runtime, "milliseconds"
-    
+  return (total_not_pipelined_runtime, total_job_runtime, min_job_cpu_millis,
+    min_job_network_millis, min_job_disk_millis)
+
+def main(argv):
+  estimate(argv[0])
 
 if __name__ == "__main__":
   main(sys.argv[1:])
